7a89fe1bc3e8be84258e71d9a5b2db95641eceab
Aklar45dev <aklar45dev@gmail.com>
2024-03-19 14:08:43 -0400

    fisrt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+const download = () => {
+  
+  axios.get('https://paperplanemaj.s3.ca-central-1.amazonaws.com/video31.mp4', {
+    responseType: 'blob',
+    onDownloadProgress: function(ProgressEvent) {
+      document.getElementById("progress").innerHTML = (Math.floor(ProgressEvent.progress*100)+"%")
+    }
+  })
+  .then((obj) => {
+    console.log(obj.data)
+    const url = URL.createObjectURL(obj.data);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = "video31.mp4";
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    URL.revokeObjectURL(url);
+  })
+  .catch(err => console.error(err))
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => download()}>Download</button>
+        <p id="progress">0%</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+//https://paperplanemaj.s3.ca-central-1.amazonaws.com/SUP.pdf
